4905ee8312e0e892b8cfc3519c2e8575b46624f2
MLiuk <92759081+marcolucchini@users.noreply.github.com>
2023-07-21 23:47:41 +0200

    Version 1 update

---
+++ precache-manifest.b6070c2accab1dc03a136034dceba72a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "39c898b8682c278cf817",
+    "url": "/static/js/main.1ed5c6f6.chunk.js"
+  },
+  {
+    "revision": "2b54d9860c9bc2bd6263",
+    "url": "/static/js/2.a2a8ea37.chunk.js"
+  },
+  {
+    "revision": "39c898b8682c278cf817",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "4bbb74bb031166268d668aa3c01c2c8e",
+    "url": "/index.html"
+  }
+];
